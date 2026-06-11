6f26d3acbac2ba166e024ffb3c94fbb4d987acef
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-06-11 21:51:34 +0600

    Update theme.js

--- frontend/js/theme.js
+++ frontend/js/theme.js
@@ -1,1 +1,33 @@
+const body=document.body;
 
+const currentTheme=
+localStorage.getItem("theme");
+
+if(currentTheme==="light"){
+
+body.classList.add("light");
+
+}
+
+function changeTheme(){
+
+body.classList.toggle("light");
+
+if(body.classList.contains("light")){
+
+localStorage.setItem(
+"theme",
+"light"
+);
+
+}
+else{
+
+localStorage.setItem(
+"theme",
+"dark"
+);
+
+}
+
+}
