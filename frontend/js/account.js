d2ad07aa266563d8fa9d88f29010392855ea8a7a
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-06-11 21:50:21 +0600

    Update account.js

--- frontend/js/account.js
+++ frontend/js/account.js
@@ -1,1 +1,38 @@
+const welcomeText=
+document.getElementById(
+"welcomeText"
+);
 
+const userEmail=
+document.getElementById(
+"userEmail"
+);
+
+welcomeText.innerText=
+"Welcome back, "+
+(
+localStorage.getItem(
+"userName"
+)
+||"User"
+);
+
+userEmail.innerText=
+localStorage.getItem(
+"userEmail"
+);
+
+
+document.getElementById(
+"logoutBtn"
+)
+.addEventListener(
+"click",
+()=>{
+
+localStorage.clear();
+
+window.location.href="./login.html";
+
+}
+);
